dd43457812894cbea2ed51865984961cb7f9a81c
Terekhov Ruslan <my.thaw@gmail.com>
2012-08-24 22:18:56 +0300

    New features

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824181417) do
+ActiveRecord::Schema.define(:version => 20120824183757) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
-    t.string "name"
-    t.string "email"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
